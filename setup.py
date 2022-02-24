42f4e742ccc6680b4259a9faf6f58b6755a8aad5
BlazenBoi <65360471+BlazenBoi@users.noreply.github.com>
2022-02-24 10:48:09 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
-long_desc = open("C:/Users/runne/OneDrive/Desktop/Bot Projects/CanvacordPy/canvacord.py/README.md", "r")
+long_desc = open("README.md", "r")
 long_description = long_desc.read()
 with open('src/canvacord/__init__.py', 'r') as f:
     version = [line.split('=')[1].strip(" '\"") for line in f.read().splitlines() if line.startswith('__version__')][0]
